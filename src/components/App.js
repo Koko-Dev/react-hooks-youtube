766b3b8e1bf40de499fbe045d9adfa36774bc8e9
Koko-Dev <koko.webdev@gmail.com>
2022-04-19 16:17:55 -0400

    App.js: Remove youtube import because we have moved that to custom hook useVideos.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from 'react';
 import SearchBar from './SearchBar';
-import youtube from '../apis/youtube';
 import VideoList  from "./VideoList";
 import VideoDetail from "./VideoDetail";
 import './App.css';
