c2caf8e9d74b1bdec3e651b1f8090084a1b732a6
Koko-Dev <koko.webdev@gmail.com>
2022-04-19 11:43:10 -0400

    Begin updating App.js and SearchBar.js from class components to functional components

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList  from "./VideoList";
 import VideoDetail from "./VideoDetail";
 import './App.css';
 
-class App extends React.Component {
-	state = { videos: [], selectedVideo: null };
+const App = () => {
+	const [videos, setVideos] = useState([]);
+	const [selectedVideo, setSelectedVideo] = useState(null);
 
-	componentDidMount() {
-		this.onTermSubmit('carrots');
-	}
-
-	onTermSubmit = async (term) => {
+	const onTermSubmit = async (term) => {
 		const response = await youtube.get('/search', {
 			params: {
 				q: term
 			}
 		});
 
-		this.setState({
-			videos: response.data.items,
-			selectedVideo: response.data.items[0]
-		});
+		setVideos(response.data.items);
+		setSelectedVideo(response.data.items[0]);
+	}
+}
+
+class App extends React.Component {
+	state = { videos: [], selectedVideo: null };
+
+	componentDidMount() {
+		this.onTermSubmit('carrots');
 	}
 
 	onVideoSelect = (video) => {
