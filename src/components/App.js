0bfb83ddb8d68ba47bc6f36d89787cb7371d3384
Koko-Dev <koko.webdev@gmail.com>
2022-04-19 16:39:08 -0400

    App.js: Change useVideos hook initialState to 'carrots'

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import useVideos from "../hooks/useVideos";
 
 const App = () => {
 	const [selectedVideo, setSelectedVideo] = useState(null);
-	const [videos, search] = useVideos('buildings');
+	const [videos, search] = useVideos('carrots');
 
 	// Note: Anytime we get a new list of videos or any
 	//  time videos changes, useEffect() will be called and
